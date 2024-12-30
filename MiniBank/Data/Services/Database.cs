5c635476cba032e5fe1c46aee3496b8efe9c7bba
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2024-12-30 10:03:36 +0330

    feat(Database): implement Delete method for Database class

--- MiniBank/Data/Services/Database.cs
+++ MiniBank/Data/Services/Database.cs
@@ -46,7 +46,22 @@ public class Database : IDataBase
     
     public void Delete<TDatabaseEntity>(TDatabaseEntity entity) where TDatabaseEntity : IDatabaseEntity
     {
-        throw new NotImplementedException();
+        var typeName = typeof(TDatabaseEntity).Name;
+        
+        if (_entities.TryGetValue(typeName, out var entityList))
+        {
+            var newEntity = entityList.Single(x=> x.Id == entity.Id);
+            var result = entityList.Remove(newEntity);
+            
+            if (result == false)
+            {
+                throw new OperationFailedException($"entity {entity.Id} of type {typeName} could not be removed or does not exist.");
+            }
+        }
+        else
+        {
+            throw new InvalidOperationException($"entity {entity.Id} of type {typeName} not found.");
+        }
     }
     
     
