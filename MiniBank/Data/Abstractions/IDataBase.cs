c9f3f45accd824529bbd0529d8df1c7fd2a77896
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2024-12-30 08:56:08 +0330

    feat: create IDataBase Interface

---
+++ MiniBank/Data/Abstractions/IDataBase.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniBank.Data.Abstractions;
+
+public interface IDataBase
+{
+    void Save<TDatabaseEntity>(TDatabaseEntity entity) where TDatabaseEntity : IDatabaseEntity;
+    
+    void Update<TDatabaseEntity>(TDatabaseEntity entity) where TDatabaseEntity : IDatabaseEntity;
+    
+    void Delete<TDatabaseEntity>(TDatabaseEntity entity) where TDatabaseEntity : IDatabaseEntity;
+    
+    IEnumerable<TDatabaseEntity> FetchAll<TDatabaseEntity>() where TDatabaseEntity : IDatabaseEntity;
+}
