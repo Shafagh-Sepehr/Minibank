4bca705c1ab2b5a903f8f997c9620d5a2fed9d45
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2024-12-30 16:08:04 +0330

    feat: create entity type Transaction

---
+++ MiniBank/Entities/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniBank.Entities;
+
+public class Transaction : ComparableDataBaseEntity
+{
+    public required string OriginAccountNumber      { get; init; }
+    public required string DestinationAccountNumber { get; init; }
+    
+    [Range(typeof(decimal), "0.000001", "79228162514264337593543950335")]
+    public required decimal Amount { get; init; }
+    
+    public required DateTime Date { get; init; } = DateTime.Now;
+}
