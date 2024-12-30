dca8c5a2ae09ad6b1898345d7d6f1905575cf5fa
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2024-12-30 14:44:40 +0330

    feat: create User Account and AccountCard classes

---
+++ MiniBank/Entities/Account.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MiniBank.Data.Abstractions;
+
+namespace MiniBank.Entities;
+
+public abstract class Account : IDatabaseEntity
+{
+    public enum AccountStatus
+    {
+        Active,
+        DeActive,
+        Blocked,
+    }
+    
+    private decimal _balance;
+    public  long    UserRef { get; set; }
+    
+    [StringLength(20, MinimumLength = 20)]
+    public required string AccountNumber { get; init; }
+    
+    public          AccountStatus Status { get; set; } = AccountStatus.Active;
+    public required AccountCard   Card   { get; init; }
+    
+    
+    public required decimal Balance
+    {
+        get => _balance;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Balance cannot be negative");
+            }
+            
+            _balance = value;
+        }
+    }
+    
+    public long Id { get; set; }
+}
---
+++ MiniBank/Entities/AccountCard.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniBank.Entities;
+
+public class AccountCard
+{
+    [StringLength(21, MinimumLength = 16)]
+    public required string CardNumber { get; set; }
+    
+    [StringLength(4, MinimumLength = 3)]
+    public required string Cvv2 { get; set; }
+    
+    public required string   PasswordHash       { get; set; }
+    public required string   SecondPasswordHash { get; set; }
+    public required DateTime ExpiryDate         { get; set; }
+}
