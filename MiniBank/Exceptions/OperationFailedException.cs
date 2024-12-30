776d197d940b27e996b30f712afe1443022795ea
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2024-12-30 09:33:45 +0330

    feat: create a new exception called OperationFailedException

---
+++ MiniBank/Exceptions/OperationFailedException.cs
@@ -0,0 +1,19 @@
+﻿namespace MiniBank.Exceptions;
+
+public class OperationFailedException : Exception
+{
+    OperationFailedException()
+    {
+        
+    }
+    
+    public OperationFailedException(string message) : base(message)
+    {
+        
+    }
+    
+    public OperationFailedException(string message, Exception inner) : base(message, inner)
+    {
+        
+    }
+}
