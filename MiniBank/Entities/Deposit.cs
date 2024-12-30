5aa85a25bcc595da334a07e8588df725d80dec81
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2024-12-30 16:13:29 +0330

    feat: create entity type Withdrawal and Deposit

---
+++ MiniBank/Entities/Deposit.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniBank.Entities;
+
+public class Deposit : ComparableDataBaseEntity
+{
+    public required string AccountNumber { get; init; }
+    
+    [Range(typeof(decimal), "0.000001", "79228162514264337593543950335")]
+    public required decimal Amount { get; init; }
+    
+    public required DateTime Date { get; init; } = DateTime.Now;
+}
