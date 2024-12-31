ca08156d40c982d27f7f27ff981ffb6b67551049
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2024-12-31 17:18:46 +0330

    feat(CardHandler): implement RequestDynamicPassword for CardHandler and also DynamicPassword class which is a DataBaseEntity

---
+++ MiniBank/Entities/Classes/DynamicPassword.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniBank.Entities.Classes;
+
+public class DynamicPassword : DataBaseEntity
+{
+    public required decimal  Amount                   { get; init; }
+    public required string   OriginCardNumber         { get; init; }
+    public          string  DestinationCardNumber    { get; init; }
+    public required string   DynamicPasswordHash      { get; init; }
+    public          DateTime ExpiryDate               { get; } = DateTime.Now.AddMinutes(2);
+}
--- MiniBank/Handlers/CardHandler.cs
+++ MiniBank/Handlers/CardHandler.cs
@@ -1,9 +1,11 @@
 ﻿using MiniBank.Data.Abstractions;
 using MiniBank.Entities.Classes;
+using MiniBank.Exceptions;
+using MiniBank.SmsService.Abstractions;
 
 namespace MiniBank.Handlers;
 
-public class CardHandler(IDataBase dataBase)
+public class CardHandler(IDataBase dataBase, ISmsService smsService)
 {
     public AccountCard CreateCard(long accountRef, string password, string secondPassword)
     {
@@ -20,6 +22,47 @@ public class CardHandler(IDataBase dataBase)
         return card;
     }
     
+    public void RequestDynamicPassword(decimal amount, string originCardNumber, string destinationCardNumber, string cvv2, DateTime expiryDate)
+    {
+        var cards = dataBase.FetchAll<AccountCard>().ToList();
+        var accounts = dataBase.FetchAll<Account>().ToList();
+        var users = dataBase.FetchAll<User>().ToList();
+        var originCard = cards.FirstOrDefault(c => c.CardNumber == originCardNumber);
+        
+        var user = (from u in users
+                    join account in accounts on u.Id equals account.UserRef
+                    join card in cards on account.Id equals card.AccountRef
+                    select u).SingleOrDefault();
+        
+        
+        if (originCard == null || originCard.Cvv2 != cvv2 || originCard.ExpiryDate.Month != expiryDate.Month ||
+            originCard.ExpiryDate.Year != expiryDate.Year)
+        {
+            throw new OperationFailedException("a card with this information couldn't be found");
+        }
+        
+        var dynamicPasswordString = Helper.GenerateRandomNumberAsString(8);
+        smsService.Send($"dynamic password: {destinationCardNumber}",user!.PhoneNumber);
+        
+        var dynamicPassword = new DynamicPassword
+        {
+            Amount = amount,
+            OriginCardNumber = originCardNumber,
+            DestinationCardNumber = destinationCardNumber,
+            DynamicPasswordHash = Helper.ComputeSha256Hash(dynamicPasswordString),
+        };
+        
+        
+        var destinationCard = cards.FirstOrDefault(c => c.CardNumber == destinationCardNumber);
+        if (destinationCard == null)
+        {
+            throw new OperationFailedException("a destination card with this number couldn't be found");
+        }
+        
+        
+        dataBase.Save(dynamicPassword);
+    }
+    
     private string GenerateCardNumber()
     {
         var cards = dataBase.FetchAll<AccountCard>().ToList();
@@ -27,7 +70,6 @@ public class CardHandler(IDataBase dataBase)
         do
         {
             cardNumber = Helper.GenerateRandomNumberAsString(16);
-            
         } while (cards.Any(x => x.CardNumber == cardNumber));
         
         return cardNumber;
