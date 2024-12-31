ce7881c21625b88c63aa97366361145e89d81445
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2024-12-31 14:31:26 +0330

    feat: create WithdrawalHandler class

---
+++ MiniBank/Handlers/WithdrawalHandler.cs
@@ -0,0 +1,27 @@
+﻿using MiniBank.Data.Abstractions;
+using MiniBank.Entities;
+
+namespace MiniBank.Handlers;
+
+public class WithdrawalHandler(IDataBase dataBase)
+{
+    public ActionResult Withdraw(string accountNumber, decimal amount)
+    {
+        var accounts = dataBase.FetchAll<Account>();
+        var account = accounts.FirstOrDefault(x => x.AccountNumber == accountNumber);
+        
+        if (account == null)
+        {
+            return ActionResult.AccountNotFound;
+        }
+        
+        account.Balance -= amount;
+        if (account.Balance < 0)
+        {
+            return ActionResult.InsufficientBalance;
+        }
+        
+        dataBase.Update(account);
+        return ActionResult.Success;
+    }
+}
