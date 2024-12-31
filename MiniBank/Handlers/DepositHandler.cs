b19e2128211d4925b16b937a03245a17471d4909
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2024-12-31 12:41:45 +0330

    feat: create DepositHandler

---
+++ MiniBank/Handlers/DepositHandler.cs
@@ -0,0 +1,24 @@
+﻿using MiniBank.Data.Abstractions;
+using MiniBank.Entities;
+
+namespace MiniBank.Handlers;
+
+public class DepositHandler(IDataBase dataBase)
+{
+    public bool Deposit(string accountNumber, decimal amount)
+    {
+        var accounts = dataBase.FetchAll<Account>();
+        
+        foreach (var account in accounts)
+        {
+            if (account.AccountNumber == accountNumber)
+            {
+                account.Balance += amount;
+                dataBase.Update(account);
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
