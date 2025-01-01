0590a7c8e195be626b0ff6663607818d9f5aaedd
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-01 19:01:18 +0330

    feat(Validators): implement more validation in AccountValidator

--- MiniBank/Validators/Services/AccountValidator.cs
+++ MiniBank/Validators/Services/AccountValidator.cs
@@ -1,30 +1,43 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using DB.Data.Abstractions;
 using MiniBank.Entities.Classes;
 
 namespace MiniBank.Validators.Services;
 
-public class AccountValidator : BaseValidator<Account>
+public class AccountValidator(IDataBase dataBase) : BaseValidator<Account>
 {
     protected override void ValidateGeneralState(Account entity, List<string> errors)
     {
         if (entity.Balance < 0)
         {
-            throw new ValidationException("Account balance cannot be less than zero");
+            errors.Add("Account balance cannot be less than zero");
+        }
+        
+        var users = dataBase.FetchAll<User>();
+        if (users.All(x => x.Id != entity.UserRef))
+        {
+            errors.Add("this account's UserRef doesn't exist");
         }
     }
     
     protected override void ValidateSaveState(Account entity, List<string> errors)
     {
-        throw new NotImplementedException();
+        if (entity.Balance != 0)
+        {
+            errors.Add("Account balance must be zero when creating it, deposit money after account creation");
+        }
     }
     
     protected override void ValidateUpdateState(Account entity, List<string> errors)
     {
-        throw new NotImplementedException();
+        
     }
     
     protected override void ValidateDeleteState(Account entity)
     {
-        throw new NotImplementedException();
+        if (entity.Balance > 1)
+        {
+            throw new ValidationException("Account balance cannot be more than 1 when deleting, withdraw money");
+        }
     }
 }
