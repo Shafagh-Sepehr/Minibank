e65875a7227f7c922f406872487e26aeb2a5a84e
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-01 10:10:04 +0330

    feat(validators): create UserValidator class

---
+++ MiniBank/Validators/Services/UserValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DataBase.Data.Abstractions;
+using MiniBank.Entities.Classes;
+
+namespace MiniBank.Validators.Services;
+
+public class UserValidator(IDataBase dataBase) : BaseValidator<User> {
+    
+    protected override void ValidateData(User entity)
+    {
+        var users = dataBase.FetchAll<User>();
+        if (users.Any(x => x.Username == entity.Username || x.NationalId == entity.NationalId))
+        {
+            throw new ValidationException("Username is already taken");
+        }
+    }
+}
