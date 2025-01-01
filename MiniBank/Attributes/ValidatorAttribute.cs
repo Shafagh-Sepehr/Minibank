801b9e73ae60cc670dadef26e466eb2b31dffd4a
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-01 10:23:43 +0330

    feat(Attributes): create ValidatorAttribute class

---
+++ MiniBank/Attributes/ValidatorAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace MiniBank.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class ValidatorAttribute(Type validatorType) : Attribute
+{
+    public Type ValidatorType { get; } = validatorType;
+}
