951914c3f59f44e8277cf2d0051abace9b5b5e72
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-01 12:26:10 +0330

    fix(Entities): remove validation from entity

--- MiniBank/Entities/Classes/Account.cs
+++ MiniBank/Entities/Classes/Account.cs
@@ -5,26 +5,12 @@ namespace MiniBank.Entities.Classes;
 
 public class Account : DataBaseEntity
 {
-    private decimal       _balance;
-    public  AccountStatus Status { get; set; } = AccountStatus.Active;
+    public required decimal       Balance { get; set; }
+    public          AccountStatus Status  { get; set; } = AccountStatus.Active;
     
     [Range(1, long.MaxValue)]
     public long UserRef { get; init; }
     
     [StringLength(20, MinimumLength = 20)]
     public required string AccountNumber { get; init; }
-    
-    public required decimal Balance
-    {
-        get => _balance;
-        set
-        {
-            if (value < 0)
-            {
-                throw new ArgumentException("Balance cannot be negative");
-            }
-            
-            _balance = value;
-        }
-    }
 }
