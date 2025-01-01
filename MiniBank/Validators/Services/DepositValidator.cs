374c11027e59ec1d80bfeb693dad5c8a6fc5b41b
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-01 18:37:18 +0330

    feat(Validators): create DepositValidator class

---
+++ MiniBank/Validators/Services/DepositValidator.cs
@@ -0,0 +1,23 @@
+﻿using MiniBank.Entities.Classes;
+
+namespace MiniBank.Validators.Services;
+
+public class DepositValidator : BaseValidator<Deposit>
+{
+    protected override void ValidateGeneralState(Deposit entity)
+    {
+        throw new NotImplementedException();
+    }
+    protected override void ValidateSaveState(Deposit entity)
+    {
+        throw new NotImplementedException();
+    }
+    protected override void ValidateUpdateState(Deposit entity)
+    {
+        throw new NotImplementedException();
+    }
+    protected override void ValidateDeleteState(Deposit entity)
+    {
+        throw new NotImplementedException();
+    }
+}
