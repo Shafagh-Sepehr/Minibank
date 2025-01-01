ee4941f57712cd3eefbee14fdf044fa6c26f2154
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-01 10:09:15 +0330

    fix(User): fix PasswordHash's data annotation and change PhoneNumber's set to init

--- MiniBank/Entities/Classes/User.cs
+++ MiniBank/Entities/Classes/User.cs
@@ -7,6 +7,7 @@ public class User : DataBaseEntity
     [StringLength(50, MinimumLength = 5)]
     public required string Username { get; set; }
     
+    [StringLength(64, MinimumLength = 64, ErrorMessage = "sha256 hash must be 64 characters.")]
     public required string PasswordHash { get; set; }
     
     [StringLength(70, MinimumLength = 2)]
@@ -16,7 +17,7 @@ public class User : DataBaseEntity
     public required string LastName { get; init; }
     
     [StringLength(11, MinimumLength = 11)]
-    public required string PhoneNumber { get; set; }
+    public required string PhoneNumber { get; init; }
     
     [StringLength(10, MinimumLength = 10)]
     public required string NationalId { get; init; }
