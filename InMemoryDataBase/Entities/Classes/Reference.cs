9c5e1953960814ab1d6f93a598065d8e84b73a1d
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-07 17:59:16 +0330

    feat(Database): create Reference class to save references in the db

---
+++ InMemoryDataBase/Entities/Classes/Reference.cs
@@ -0,0 +1,9 @@
+﻿namespace InMemoryDataBase.Entities.Classes;
+
+public class Reference
+{
+    public required Type   MasterType { get; set; }
+    public required Type   SlaveType  { get; set; }
+    public required string MasterId   { get; set; }
+    public required string SlaveId    { get; set; }
+}
