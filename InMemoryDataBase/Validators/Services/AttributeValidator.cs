240a453ce68d1fe7b8268a00d459d8bc13f17d83
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-20 11:08:47 +0330

    refactor(Database): refactor attribute count validation which could be shrunk

--- InMemoryDataBase/Validators/Services/AttributeValidator.cs
+++ InMemoryDataBase/Validators/Services/AttributeValidator.cs
@@ -14,44 +14,9 @@ public class AttributeValidator : IAttributeValidator
         
         foreach (var propertyInfo in properties)
         {
-            var count = 0;
-            var hasPrimaryAttribute = false;
-            var hasForeignAttribute = false;
-            var hasDefaultValueAttribute = false;
-            var hasNullableAttribute = false;
-            
-            if (propertyInfo.GetCustomAttribute(typeof(PrimaryKeyAttribute), true) != null)
+            if(propertyInfo.CustomAttributes.Count() > 1 && propertyInfo.CustomAttributes.All(a=>a.AttributeType == typeof(ForeignKeyAttribute) || a.AttributeType == typeof(NullableAttribute)))
             {
-                count++;
-                hasPrimaryAttribute = true;
-            }
-            
-            if (propertyInfo.GetCustomAttribute(typeof(ForeignKeyAttribute), true) != null)
-            {
-                count++;
-                hasForeignAttribute = true;
-            }
-            
-            if (propertyInfo.GetCustomAttribute(typeof(DefaultValueAttribute), true) != null)
-            {
-                count++;
-                hasDefaultValueAttribute = true;
-            }
-            
-            if (propertyInfo.GetCustomAttribute(typeof(NullableAttribute), true) != null)
-            {
-                count++;
-                hasNullableAttribute = true;
-            }
-            
-            switch (count)
-            {
-                case <= 1:
-                    continue;
-                case 2 when hasForeignAttribute && hasNullableAttribute:
-                    return;
-                default:
-                    throw new DatabaseException($"type `{type}`'s `{propertyInfo.Name}` property can't have more than one attribute");
+                throw new DatabaseException($"type `{type}`'s `{propertyInfo.Name}` property can't have more than one attribute");
             }
         }
     }
