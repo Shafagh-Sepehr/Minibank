a1c8fc165d1f5a9ae6df922871bca24a2a457ebf
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-21 09:15:35 +0330

    refactor(MiniBank): shorten the ComputeSha256Hash method (use Convert.ToHexString() instead of the loop)

--- MiniBank/Helper.cs
+++ MiniBank/Helper.cs
@@ -24,15 +24,8 @@ public static class Helper
         // Create a SHA256
         // ComputeHash - returns byte array
         var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(rawData));
-        
+
         // Convert byte array to a string
-        var builder = new StringBuilder();
-        
-        foreach (var b in bytes)
-        {
-            builder.Append(b.ToString("x2"));
-        }
-        
-        return builder.ToString();
+        return Convert.ToHexString(bytes).ToLower();
     }
 }
