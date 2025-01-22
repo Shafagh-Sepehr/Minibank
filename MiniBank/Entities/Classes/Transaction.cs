34f2fe6503a5227a83ad6291b56df9adec40e9f6
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-22 10:01:03 +0330

    feat(MiniBank): add origin and destination account number to transaction entity

--- MiniBank/Entities/Classes/Transaction.cs
+++ MiniBank/Entities/Classes/Transaction.cs
@@ -8,15 +8,19 @@ namespace MiniBank.Entities.Classes;
 [Validator(typeof(IValidator<Account>), typeof(Account))]
 public class Transaction : DataBaseEntity
 {
-    public required decimal           Amount      { get; init; }
-    public required TransactionStatus Status      { get; init; }
-    public required TransactionType   Type        { get; init; }
-    public          DateTime          Date        { get; } = DateTime.Now;
-    public          string?           Description { get; init; }
-    
+    public required decimal Amount { get; init; }
+    public required TransactionStatus Status { get; init; }
+    public required TransactionType Type { get; init; }
+    public DateTime Date { get; } = DateTime.Now;
+    public string? Description { get; init; }
+
     [Range(1, long.MaxValue)]
     public required long OriginAccountRef { get; init; }
-    
+    [StringLength(20, MinimumLength = 20)]
+    public required string OriginAccountNumber { get; init; }
+
     [Range(1, long.MaxValue)]
     public required long DestinationAccountRef { get; init; }
+    [StringLength(20, MinimumLength = 20)]
+    public required string DestinationAccountNumber { get; init; }
 }
