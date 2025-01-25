e0d8cd7b43a99336c7dab001e71933ab8730d4c2
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-25 11:10:24 +0330

    refactor(Abstractions): fix parameter name case

--- Abstractions/Repository/IRepository.cs
+++ Abstractions/Repository/IRepository.cs
@@ -3,7 +3,7 @@
 public interface IRepository
 {
     List<T> FetchAll<T>() where T : DataBaseEntity;
-    T Read<T>(string Id) where T : DataBaseEntity;
+    T Read<T>(string id) where T : DataBaseEntity;
     T Insert<T>(T entity) where T : DataBaseEntity;
     T Update<T>(T entity) where T : DataBaseEntity;
     T Delete<T>(T entity) where T : DataBaseEntity;
