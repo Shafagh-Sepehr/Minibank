9a205cfed499cfe9b05563f57505f8cef80677e1
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-25 13:44:33 +0330

    feat(Repository): implement AccountInMemoryRepository

--- Repository/InMemoryRepository/Services/AccountInMemoryRepository.cs
+++ Repository/InMemoryRepository/Services/AccountInMemoryRepository.cs
@@ -1,32 +1,64 @@
-﻿using MiniBank.Entities.Classes;
+﻿using Abstractions.InMemoryDatabase;
+using InMemoryDataBase.Core.Abstractions;
+using MiniBank.Entities.Classes;
 using Repository.Abstractions;
+using Repository.Data;
 
 namespace Repository.InMemoryRepository.Services;
 
-public class AccountInMemoryRepository : IEntityRepository<Account>
+public class AccountInMemoryRepository(IShafaghDB shafaghDB) : IEntityRepository<Account>
 {
     public List<Account> FetchAll()
     {
-        throw new NotImplementedException();
+        return shafaghDB.FetchAll<AccountDao>().Select(AccountToDao).ToList();
     }
 
     public Account? FetchById(string id)
     {
-        throw new NotImplementedException();
+        var accountDao = shafaghDB.FetchById<AccountDao>(id);
+        return accountDao == null ? null : AccountToDao(accountDao);
     }
 
     public void Insert(Account entity)
     {
-        throw new NotImplementedException();
+        var accountDao = DaoToAccount(entity);
+        shafaghDB.Insert(accountDao);
     }
 
     public void Update(Account entity)
     {
-        throw new NotImplementedException();
+        var accountDao = DaoToAccount(entity);
+        ((IVersionable)accountDao).Version = ((IVersionable)accountDao).Version;
+        shafaghDB.Update(accountDao);
     }
 
     public void Delete(string id)
     {
-        throw new NotImplementedException();
+        shafaghDB.Delete<AccountDao>(id);
+    }
+
+    private static AccountDao DaoToAccount(Account account)
+    {
+        return new AccountDao
+        {
+            AccountNumber = account.AccountNumber,
+            Balance = account.Balance,
+            Status = account.Status,
+            UserRef = account.UserRef,
+            Id = account.Id,
+        };
+    }
+
+    private static Account AccountToDao(AccountDao accountDao)
+    {
+        var account = new Account
+        {
+            AccountNumber = accountDao.AccountNumber,
+            Status = accountDao.Status,
+            UserRef = accountDao.UserRef,
+            Id = accountDao.Id,
+        };
+        account.IncreaseBalance(accountDao.Balance);
+        return account;
     }
 }
