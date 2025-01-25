2abae1b0d42813a52d6300372989c290c78c8152
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-25 12:32:19 +0330

    refactor(Repository): convert methods' body into expression body

--- Repository/InMemoryRepository/InMemoryRepository.cs
+++ Repository/InMemoryRepository/InMemoryRepository.cs
@@ -6,30 +6,15 @@ namespace Repository.InMemoryRepository;
 
 public class InMemoryRepository : IRepository
 {
-    public List<T> FetchAll<T>() where T : DataBaseEntity
-    {
-        return GetEntityRepository<T>().FetchAll();
-    }
+    public List<T> FetchAll<T>() where T : DataBaseEntity => GetEntityRepository<T>().FetchAll();
 
-    public T FetchById<T>(string id) where T : DataBaseEntity
-    {
-        return GetEntityRepository<T>().FetchById(id);
-    }
+    public T FetchById<T>(string id) where T : DataBaseEntity => GetEntityRepository<T>().FetchById(id);
 
-    public T Insert<T>(T entity) where T : DataBaseEntity
-    {
-        return GetEntityRepository<T>().Insert(entity);
-    }
+    public T Insert<T>(T entity) where T : DataBaseEntity => GetEntityRepository<T>().Insert(entity);
 
-    public T Update<T>(T entity) where T : DataBaseEntity
-    {
-        return GetEntityRepository<T>().Update(entity);
-    }
+    public T Update<T>(T entity) where T : DataBaseEntity => GetEntityRepository<T>().Update(entity);
 
-    public T Delete<T>(string id) where T : DataBaseEntity
-    {
-        return GetEntityRepository<T>().Delete(id);
-    }
+    public T Delete<T>(string id) where T : DataBaseEntity => GetEntityRepository<T>().Delete(id);
 
     private static IEntityRepository<T> GetEntityRepository<T>() where T : DataBaseEntity
     => ServiceCollection.ServiceProvider.GetRequiredService<IEntityRepository<T>>();
