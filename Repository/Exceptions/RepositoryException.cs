1a4815b570711dfd5c6a316256fbd9cdecc46465
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-01-25 11:58:13 +0330

    feat(Repository): create repository exception

---
+++ Repository/Exceptions/RepositoryException.cs
@@ -0,0 +1,18 @@
+﻿namespace Repository.Exceptions;
+
+public class RepositoryException : Exception
+{
+    RepositoryException()
+    {
+    }
+    
+    public RepositoryException(string message) : base(message)
+    {
+        
+    }
+    
+    public RepositoryException(string message, Exception inner) : base(message, inner)
+    {
+        
+    }
+}
